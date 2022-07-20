df9d09236df94e0a99a2fbba466bbb0a470c578d
Sinthuja Nagalingam <sinthuja@popsql.com>
2022-07-19 19:17:24 -0700

    initial contract and tests

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+provider = new HDWalletProvider(
+  'cup shift frequent chair fame member dove spend crucial use next double',
+  'https://rinkeby.infura.io/v3/edab43b689c14a7aa0108e4075f67b2c'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
